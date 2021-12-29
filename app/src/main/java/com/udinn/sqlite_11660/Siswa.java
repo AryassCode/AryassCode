304cbfe815fd50ea4d368351de188676fb514cbc
AryassCode <gynsf234@gmail.com>
2021-12-29 19:12:59 +0700

    first commit

---
+++ app/src/main/java/com/udinn/sqlite_11660/Siswa.java
@@ -0,0 +1,22 @@
+package com.udinn.sqlite_11660;
+
+public class Siswa {
+    String nim;
+    String nama_siswa;
+
+    public Siswa(){
+
+    }
+    public String getNim(){
+        return nim;
+    }
+    public void SetNim(String nim){
+        this.nim = nim;
+    }
+    public String getNama_siswa(){
+        return nama_siswa;
+    }
+    public void SetNama_siswa(String nama_siswa){
+        this.nama_siswa = nama_siswa;
+    }
+}
